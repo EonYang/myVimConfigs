66ced21b9f57e09eacf073edf10854970874ff81
yangyang <yangeon@163.com>
2018-04-01 14:12:24 -0400

    smooth roll

---
+++ install_bundles.sh
@@ -0,0 +1,27 @@
+# jslint
+npm i -g jslint
+
+# syntastic
+cd ~/.vim/bundle && \
+git clone --depth=1 https://github.com/vim-syntastic/syntastic.git
+
+# nerdtree
+git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
+
+# tern
+cd ~/.vim/bundle
+git clone https://github.com/ternjs/tern_for_vim.git
+cd ~/.vim/bundle/tern_for_vim
+npm install
+
+
+# you complete me
+cd ~/.vim/bundle
+git clone https://github.com/Valloric/YouCompleteMe.git
+
+cd ~/.vim/bundle/YouCompleteMe
+./install.py
+
+# vim-javascript
+git clone https://github.com/pangloss/vim-javascript.git ~/.vim/bundle/vim-javascript
+
